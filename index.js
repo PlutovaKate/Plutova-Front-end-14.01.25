4d984f8a9dc2207e8c57e1a67a95f3f65d63c18d
Kateryna Plutova <plutova0207@gmail.com>
2025-02-24 15:32:53 +0200

    add HW-12

--- index.js
+++ index.js
@@ -1,91 +1,29 @@
-// ДЗ 11. Аналізуємо масив об’єктів
+// ДЗ 12. Функція removeElementFromArray
 
-// Дано масив об’єктів. Вивести масив телефонних номерів користувачів в яких баланс більше ніж 2000 доларів.
-// І знайти суму всіх балансів користувачів
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
-let users = [
-  {
-    index: 0,
-    isActive: true,
-    balance: "$2,226.60",
-    name: "Eugenia Sawyer",
-    gender: "female",
-    phone: "+1 (840) 583-3207",
-    address: "949 John Street, Rose, Puerto Rico, 1857",
-  },
-  {
-    index: 1,
-    isActive: true,
-    balance: "$2,613.77",
-    name: "Pauline Gallegos",
-    gender: "female",
-    phone: "+1 (985) 593-3328",
-    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
-  },
-  {
-    index: 2,
-    isActive: false,
-    balance: "$3,976.41",
-    name: "Middleton Chaney",
-    gender: "male",
-    phone: "+1 (995) 591-2478",
-    address: "807 Fleet Walk, Brutus, Arkansas, 9783",
-  },
-  {
-    index: 3,
-    isActive: true,
-    balance: "$1,934.58",
-    name: "Burns Poole",
-    gender: "male",
-    phone: "+1 (885) 559-3422",
-    address: "730 Seba Avenue, Osage, Alabama, 6290",
-  },
-  {
-    index: 4,
-    isActive: true,
-    balance: "$3,261.65",
-    name: "Mcfadden Horne",
-    gender: "male",
-    phone: "+1 (942) 565-3988",
-    address: "120 Scholes Street, Kirk, Michigan, 1018",
-  },
-  {
-    index: 5,
-    isActive: false,
-    balance: "$1,790.56",
-    name: "Suzette Lewis",
-    gender: "female",
-    phone: "+1 (837) 586-3283",
-    address: "314 Dunne Place, Bawcomville, Guam, 9053",
-  },
-];
+console.log("варіант 1:");
 
-let balanceUser;
-let sumAllBalance = 0;
-let sumBiggerBalance = 0;
-let usersNumbers = [];
-
-for (const key in users) {
-  balanceUser = Number(
-    users[key].balance.replaceAll(",", "").replaceAll("$", "")
-  );
-
-  sumAllBalance += balanceUser;
-
-  if (balanceUser > 2000) {
-    usersNumbers.push(users[key].phone);
-    sumBiggerBalance += balanceUser;
+const removeElement = function (array, index) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (i === index - 1) {
+      array.splice(i, 1);
+    }
   }
-}
+  return array;
+};
 
-sumAllBalance = sumAllBalance.toFixed(2);
-console.log(`Сума всіх балансів користувачів ${sumAllBalance}$`);
-console.log(
-  `Масив телефонних номерів користувачів в яких баланс більше ніж 2000 доларів:`
-);
-console.log(usersNumbers);
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
 
-sumBiggerBalance = sumBiggerBalance.toFixed(2);
-console.log(
-  `Сума балансів користувачів в яких баланс більше ніж 2000 доларів: ${sumBiggerBalance}$`
-);
+console.log("варіант 2:");
+
+const removeElement2 = function (array, index) {
+  return array.filter((element) => element !== index);
+};
+
+const newArray = removeElement2(array, 5);
+console.log(newArray);
+// // Результат: [1, 2, 3, 4, 6, 7]
