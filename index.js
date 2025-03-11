0eb56a81f936b98da5d6e3289cd06909b391189b
Kateryna Plutova <plutova0207@gmail.com>
2025-03-11 14:55:59 +0200

    add HW-16

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-// ДЗ 15. Рекурсивне возведення в ступінь
+// ДЗ 16. Кешування дзвінків
 
-// Реалізувати рекурсивну функцію, яка зводить число в ступінь.
+// Вам потрібно написати функцію, яка як параметр приймає функцію і додає їй можливість кешувати дзвінки.
+//  Ідея полягає в тому, що при виклику функції з однаковими аргументами немає сенсу викликати функцію щоразу,
+//  достатньо зберігати дані про результати виклику.
 
-// Число, яке потрібно звести в ступінь, передається як перший аргумент у функцію
+// Зберігати потрібно останні 10 дзвінків.
 
-// Ступінь передається як другий аргумент у функцію
+function saveCalls(fn) {
+  const lastTenNumbers = [];
 
-// pow(num, degree)
+  return function (phoneNumber) {
+    if (!lastTenNumbers.includes(phoneNumber)) {
+      lastTenNumbers.push(phoneNumber);
+    }
 
-function pow(num, degree) {
-  if (degree === 0) {
-    return 1;
-  }
+    if (lastTenNumbers.length > 10) {
+      lastTenNumbers.shift();
+    }
 
-  if (degree < 0) {
-    return 1 / pow(num, -degree);
-  }
-
-  return num * pow(num, degree - 1);
+    console.log(`Останні 10 номерів ${lastTenNumbers}`);
+    return fn(phoneNumber);
+  };
 }
 
-console.log(pow(2, 0));
-console.log(pow(2, -2));
-console.log(pow(3, 2));
+function cacheNumbers(phoneNumber) {
+  return phoneNumber;
+}
+
+const cachedNumbers = saveCalls(cacheNumbers);
+
+console.log(cachedNumbers("099-999-99-99"));
+console.log(cachedNumbers("098-888-88-88"));
+console.log(cachedNumbers("097-777-77-77"));
+console.log(cachedNumbers("096-666-66-66"));
+console.log(cachedNumbers("098-555-55-55"));
+console.log(cachedNumbers("097-444-44-44"));
+console.log(cachedNumbers("096-333-33-33"));
+console.log(cachedNumbers("096-222-22-22"));
+console.log(cachedNumbers("098-111-11-11"));
+console.log(cachedNumbers("097-777-77-77"));
+console.log(cachedNumbers("096-666-66-66"));
+console.log(cachedNumbers("096-000-00-00"));
