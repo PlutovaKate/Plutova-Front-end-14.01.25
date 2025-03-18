1ace5df0c46e1669bb8b3a2af717fece31e703b4
Kateryna Plutova <plutova0207@gmail.com>
2025-03-18 19:43:26 +0200

    finish first part of HW

--- index.js
+++ index.js
@@ -6,58 +6,75 @@
 //  вивести на сторінку підсумкову вартість покупки. Якщо вартість перевищує 10.000грн,
 // розрахувати знижку в 20% і повідомити про це користувача.
 
-const goods = [
-  { number: 1, name: "televisor", price: "25000 grn", quantity: 25 },
-  { number: 2, name: "fridge", price: "16000 grn", quantity: 13 },
-  { number: 3, name: "mobile - phone", price: "24000 grn", quantity: 22 },
-  { number: 4, name: "iron", price: "5000 grn", quantity: 46 },
-  { number: 5, name: "hair - dryer", price: "3000 grn", quantity: 18 },
+const products = [
+  { id: 1, name: "телевізор", price: 25000, quantity: 25 },
+  { id: 2, name: "холодильник", price: 16000, quantity: 13 },
+  { id: 3, name: "мобільний телефон", price: 24000, quantity: 22 },
+  { id: 4, name: "праска", price: 5000, quantity: 46 },
+  { id: 5, name: "фен", price: 3000, quantity: 18 },
 ];
 
-goods.map((product) => console.log(product));
+console.log("Список товарів:");
+products.map((product) =>
+  console.log(
+    `${product.id}. ${product.name} - ${product.price} грн, ${product.quantity} шт`
+  )
+);
 
-function showProduct(
-  allProducts,
-  numberOfProductFromUser,
-  quantityOfProductFromUser
-) {
-  allProducts.map((product) => {
-    if (Number(numberOfProductFromUser) === product.number) {
-      console.log(
-        `Номер ${numberOfProductFromUser} відповідає товару ${product.name}, який коштує ${product.price} за одиницю`
-      );
+function isValidProductById() {
+  let value = Number(prompt("Введіть номер товару від 1 до 5"));
 
-      if (Number(quantityOfProductFromUser) <= product.quantity) {
-        let productPrise =
-          Number(quantityOfProductFromUser) * Number.parseFloat(product.price);
+  while (isNaN(value) || !products.some((product) => product.id === value)) {
+    console.log(
+      "Нажаль такого товару немає в наявності. Введіть будь-ласка номер товару від 1 до 5"
+    );
+    alert(
+      "Нажаль такого товару немає в наявності. Введіть будь-ласка номер товару від 1 до 5"
+    );
+    value = Number(prompt("Введіть номер товару від 1 до 5"));
+  }
 
-        console.log(
-          `${quantityOfProductFromUser} шт по ${product.price} за одиницю. Оформлюємо замовлення на загальну суму ${productPrise}`
-        );
-
-        if (productPrise > 10000) {
-          let productPriseWithDiscount = productPrise - productPrise * 0.2;
-
-          console.log(
-            `Сума вашого замовлення більше 10 000грн, тому ви отримаєте знижку 20%. Сума замовлення зі знижкою буде ${productPriseWithDiscount} грн`
-          );
-        }
-      } else {
-        console.log(
-          `${quantityOfProductFromUser} товарів не має в наявності. Залишилось ${product.quantity}`
-        );
-      }
-    } else if (Number(numberOfProductFromUser) > 5) {
-      console.log(
-        "нажаль такого товару немає, спробуйте вибрати інший від 1 до 5"
-      );
-    } else {
-      console.log("Щось пішло не так...");
-    }
-  });
+  return value;
 }
 
-showProduct(goods, 5, 1);
+function isValidProductByQuantity(selectedProduct) {
+  let value = prompt(
+    `Введіть кількість товару (доступно ${selectedProduct.quantity} шт.):`
+  );
+
+  while (
+    isNaN(value) ||
+    Number(value) <= 0 ||
+    Number(value) > selectedProduct.quantity
+  ) {
+    alert(
+      `Некоректна кількість! Введіть число від 1 до ${selectedProduct.quantity}.`
+    );
+    value = prompt(
+      `Введіть кількість товару (доступно ${selectedProduct.quantity} шт.):`
+    );
+  }
+
+  return Number(value);
+}
+
+const productId = isValidProductById();
+const selectedProduct = products.find((product) => product.id === productId);
+const productQuantity = isValidProductByQuantity(selectedProduct);
+
+let totalPrice = productQuantity * selectedProduct.price;
+let discount = 0;
+let message = "";
+
+if (totalPrice > 10000) {
+  discount = totalPrice * 0.2;
+  totalPrice -= discount;
+  message = `Ваша покупка більше 10 000 грн, тому ваша знижка складає 20%. Сума до сплати з урахуванням знижки ${totalPrice}`;
+
+  console.log(message);
+
+  alert(message);
+}
 
 // * ускладнити практичне завдання запровадженням категорій товарів. Відповідно,
 //  користувач може вибрати категорію товару, номер товару та кількість.
