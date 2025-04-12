ec0c3145ee9b2779e60859b5ac82cb646cedd74a
Kateryna Plutova <plutova0207@gmail.com>
2025-04-12 22:54:56 +0300

    finish HW-32

--- index.js
+++ index.js
@@ -1,70 +1,123 @@
-// ДЗ 31. Гамбургер
+// ДЗ 32. Журнал студентів
 
-// Мережа фастфудів пропонує кілька видів гамбургерів:
+// Вам потрібно зробити конструктор сутності "Студент".
 
-// маленький (50 тугриків, 20 калорій)
-// великий (100 тугриків, 40 калорій)
-// Гамбургер може бути з одним із кількох видів начинок:
+// Студент має ім'я, прізвище, рік народження — це властивості. Є масив з оцінками, це також властивість.
+//  І є можливість отримати вік студента та його середній бал – це методи.
 
-// сиром (+ 10 тугриків, + 20 калорій)
-// салатом (+ 20 тугриків, + 5 калорій)
-// картоплею (+ 15 тугриків, + 10 калорій)
-// Можна додати добавки:
+// Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, спочатку він не заповнений,
+//  але на 25 елементів. Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо метод .present() на
+//  чергове порожнє місце, в масив записується true, коли викликаємо .absent() - записується false. Передбачте будь-який
+// захист від того, щоб у масиві відвідуваності не могло бути більше ніж 25 записів. Масив – це властивість, present та
+//  absent – ​​методи.
 
-// посипати приправою (+ 15 тугриків, 0 калорій)
-// полити майонезом (+ 20 тугриків, + 5 калорій).
-// Напишіть програму, яка розраховує вартість та калорійність гамбургера.
-//  Використовуй ОВП підхід (підказка: необхідний клас Гамбургер, константи, способи вибору опцій і
-// розрахунку необхідних величин).
+// Останній метод: .summary(), перевіряє середню оцінку і середнє відвідування(кількістьВідвідин/кількістьЗанять),
+//  і якщо середня оцінка більше ніж 90, а середнє відвідування більше 0.9, то метод summary повертає рядок "Молодець!",
+//  якщо одне з цих значень менше , то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
 
-class Hamburger {
-  static SIZE_SMALL = { price: 50, calories: 20 };
-  static SIZE_BIG = { price: 100, calories: 40 };
+// Не забудьте після того, як напишете цей конструктор, створити 2-3 екземпляри (конкретних студентів) і
+//  показати використання цих методів.
 
-  static STUFFING_CHEESE = { price: 10, calories: 20 };
-  static STUFFING_SALAD = { price: 20, calories: 5 };
-  static STUFFING_POTATO = { price: 15, calories: 10 };
-
-  static TOPPING_SAUCE = { price: 15, calories: 0 };
-  static TOPPING_MAYO = { price: 20, calories: 5 };
-
-  constructor(size, stuffing) {
-    this.size = size;
-    this.stuffing = stuffing;
-    this.toppings = [];
+class Student {
+  constructor({ name, surName, birth, grades } = {}) {
+    (this.name = name),
+      (this.surName = surName),
+      (this.birth = birth),
+      (this.grades = grades);
+    this.attendance = new Array(25).fill(null);
+    this.currentLesson = 0;
   }
 
-  addTopping(topping) {
-    this.toppings.push(topping);
+  getAge() {
+    let year = new Date().getFullYear();
+    return year - this.birth;
   }
 
-  calculatePrice() {
-    let totalPrice = this.size.price + this.stuffing.price;
-    this.toppings.forEach((topping) => (totalPrice += topping.price));
-    return totalPrice;
+  getAverageGrades() {
+    let gradeLength = this.grades.length;
+    let total = this.grades.reduce((sum, grade) => {
+      return (sum += grade);
+    }, 0);
+
+    return total / gradeLength;
   }
 
-  calculateCalories() {
-    let totalCalories = this.size.calories + this.stuffing.calories;
-    this.toppings.forEach((topping) => (totalCalories += topping.calories));
-    return totalCalories;
+  present() {
+    if (this.currentLesson < 25) {
+      this.attendance[this.currentLesson] = true;
+      this.currentLesson += 1;
+    }
+  }
+
+  absent() {
+    if (this.currentLesson < 25) {
+      this.attendance[this.currentLesson] = false;
+      this.currentLesson += 1;
+    }
+  }
+
+  summary() {
+    const middleGrade = this.getAverageGrades();
+    const quantityAttendance = this.attendance.filter(
+      (att) => att === true
+    ).length;
+    const quantityLessons = this.attendance.filter(
+      (att) => att !== null
+    ).length;
+    const middleAtendance = quantityAttendance / quantityLessons;
+
+    if (middleGrade > 90 && middleAtendance > 0.9) {
+      return "Молодець!";
+    } else if (middleGrade > 90 || middleAtendance > 0.9) {
+      return "Добре, але можна краще!";
+    } else {
+      return "Редиска!";
+    }
   }
 }
 
-// Приклад роботи коду:
-// маленький гамбургер із начинкою із сиру
-const hamburger = new Hamburger(
-  Hamburger.SIZE_SMALL,
-  Hamburger.STUFFING_CHEESE
-);
-console.log(hamburger);
-// Добавка з майонезу
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
-// Запитаємо скільки там калорій
-console.log("Calories: " + hamburger.calculateCalories());
-// скільки коштує
-console.log("Price: " + hamburger.calculatePrice());
-// я тут передумав і вирішив додати ще приправу
-hamburger.addTopping(Hamburger.TOPPING_SAUCE);
-// А скільки тепер коштує?
-console.log("Price with sauce: " + hamburger.calculatePrice());
+const student_1 = new Student({
+  name: "Kate",
+  surName: "Plutova",
+  birth: 1987,
+  grades: [90, 91, 92, 96],
+});
+
+student_1.present();
+student_1.absent();
+student_1.present();
+student_1.present();
+student_1.absent();
+student_1.summary();
+
+console.log(`${student_1.name} ${student_1.surName}: ${student_1.summary()}`);
+
+const student_2 = new Student({
+  name: "Alex",
+  surName: "Fesenko",
+  birth: 1976,
+  grades: [99, 98, 100, 100],
+});
+
+student_2.present();
+student_2.present();
+student_2.present();
+student_2.present();
+student_2.present();
+
+console.log(`${student_2.name} ${student_2.surName}: ${student_2.summary()}`);
+
+const student_3 = new Student({
+  name: "Joe",
+  surName: "Mango",
+  birth: 2006,
+  grades: [58, 48, 80, 20],
+});
+
+student_3.present();
+student_3.present();
+student_3.absent();
+student_3.absent();
+student_3.absent();
+
+console.log(`${student_3.name} ${student_3.surName}: ${student_3.summary()}`);
